1c4d06ede9d8a7f23f5a95af22fca7efb0025ddf
Lasse <lasse100204@gmail.com>
2022-01-27 15:10:22 +0100

    asteroids v.3

--- game/sketch.js
+++ game/sketch.js
@@ -1,7 +1,7 @@
 var ship;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(600, 600);
   ship = new Ship();
 }
 
@@ -11,36 +11,37 @@ function draw() {
   ship.turn();
 }
 
-  function keyReleased() {
-    ship.setRotaion(0);
+function keyReleased() {
+  ship.setRotation(0);
+}
+function keyPressed() {
+  if (keyCode == RIGHT_ARROW) {
+    ship.setRotation(0.1);
+  } else if (keyCode == LEFT_ARROW) {
+    ship.setRotation(-0.1);
   }
-  function keyPressed() {
-    if(keyCode == RIGHT_ARROW) {
-      ship.setRotation(0.1);
-    } else if (keyCode == LEFT_ARROW) {
-      ship.setRotation(.0,1);
-    }
-  }
+}
+
 //creating ship and giving it a starting position
 function Ship() {
-  this.pos = createVector(width/2, height/2);
+  this.pos = createVector(width / 2, height / 2);
   this.r = 15;
   this.heading = 0;
   this.rotation = 0;
 
 
-  this.render = function() {
+  this.render = function () {
     translate(this.pos.x, this.pos.y);
-    rotate(this.heading)
+    rotate(this.heading);
     fill(227, 2, 2);
     stroke(255);
-    triangle(-this.r, this.r, this.r, this.r, 0, -this.r )
+    triangle(-this.r, this.r, this.r, this.r, 0, -this.r)
   }
 
-    this.setRotation = function() {
-      this.rotation = a;
-    }
-  this.turn = function() {
+  this.setRotation = function (a) {
+    this.rotation = a;
+  }
+  this.turn = function () {
     this.heading += this.rotation;
   }
 }
