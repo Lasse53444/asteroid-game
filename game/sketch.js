d9bbfe296117ed63fc5ed6bbf99c2d828702c038
Lasse <lasse100204@gmail.com>
2022-01-27 15:00:03 +0100

    asteriods v.2

--- game/sketch.js
+++ game/sketch.js
@@ -8,13 +8,39 @@ function setup() {
 function draw() {
   background(0);
   ship.render();
+  ship.turn();
 }
+
+  function keyReleased() {
+    ship.setRotaion(0);
+  }
+  function keyPressed() {
+    if(keyCode == RIGHT_ARROW) {
+      ship.setRotation(0.1);
+    } else if (keyCode == LEFT_ARROW) {
+      ship.setRotation(.0,1);
+    }
+  }
 //creating ship and giving it a starting position
 function Ship() {
   this.pos = createVector(width/2, height/2);
-  this.r = 10;
+  this.r = 15;
+  this.heading = 0;
+  this.rotation = 0;
+
+
   this.render = function() {
-   triangle(-this.r, this.r, this.r, this.r, 0, -this.r )
+    translate(this.pos.x, this.pos.y);
+    rotate(this.heading)
+    fill(227, 2, 2);
+    stroke(255);
+    triangle(-this.r, this.r, this.r, this.r, 0, -this.r )
   }
 
+    this.setRotation = function() {
+      this.rotation = a;
+    }
+  this.turn = function() {
+    this.heading += this.rotation;
+  }
 }
