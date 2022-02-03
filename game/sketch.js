3b946f85ef4cccaaa1dbdd2b66a7ae0b286df2a1
Lasse <lasse100204@gmail.com>
2022-02-03 14:12:50 +0100

    display asteroids

--- game/sketch.js
+++ game/sketch.js
@@ -1,47 +1,35 @@
 var ship;
+var asteroids = [];
 
 function setup() {
   createCanvas(600, 600);
   ship = new Ship();
+  asteroids.push(new Asteroid());
 }
 
 function draw() {
   background(0);
   ship.render();
   ship.turn();
+  ship.update();
+  ship.edges();
+
+  for (var i = 0; i < asteroids.length; i++) {
+    asteroids[i].render();
+  }
 }
 
 function keyReleased() {
   ship.setRotation(0);
+  ship.boosting(false);
 }
+
 function keyPressed() {
   if (keyCode == RIGHT_ARROW) {
     ship.setRotation(0.1);
   } else if (keyCode == LEFT_ARROW) {
     ship.setRotation(-0.1);
-  }
-}
-
-//creating ship and giving it a starting position
-function Ship() {
-  this.pos = createVector(width / 2, height / 2);
-  this.r = 15;
-  this.heading = 0;
-  this.rotation = 0;
-
-
-  this.render = function () {
-    translate(this.pos.x, this.pos.y);
-    rotate(this.heading);
-    fill(227, 2, 2);
-    stroke(255);
-    triangle(-this.r, this.r, this.r, this.r, 0, -this.r)
-  }
-
-  this.setRotation = function (a) {
-    this.rotation = a;
-  }
-  this.turn = function () {
-    this.heading += this.rotation;
+  } else if (keyCode == UP_ARROW) {
+    ship.boosting(true);  
   }
 }
